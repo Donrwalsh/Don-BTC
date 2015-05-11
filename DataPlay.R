4ab64a428b2a41aac06137babb3de998cb2ce031
Donrwalsh <donrwalsh@gmail.com>
2015-05-10 23:04:11 -0500

    tweaks

--- DataPlay.R
+++ DataPlay.R
@@ -1,4 +1,10 @@
-portfolio <- data.frame(read.csv("/Users/don/Desktop/BTC/portfolio.csv", header=TRUE, stringsAsFactors=FALSE))
+Begin <- function(){
+  portfolio <<- data.frame(read.csv("/Users/don/Desktop/BTC/portfolio.csv", header=TRUE, stringsAsFactors=FALSE))
+  op <<- sum(as.numeric(portfolio$pos)) # = overall position
+  oi <<- sum(as.numeric(portfolio$pos)*as.numeric(portfolio$price)) # = overall investment
+  pb <<- oi/op # = price basis
+}
+Begin()
 
 #example of adding a row to portfolio:
 newvec <- c("12/8/13",0.0433,806.70)
@@ -7,10 +13,7 @@ portfolio <- rbind(portfolio,newvec)
 #save portfolio:
 write.csv(portfolio, file = "/Users/don/Desktop/BTC/portfolio.csv", row.names=FALSE)
 
-#determine price basis:
-op <- sum(as.numeric(portfolio$pos)) # = overall position
-oi <- sum(as.numeric(portfolio$pos)*as.numeric(portfolio$price)) # = overall investment
-pb <- oi/op # = OI/OP
+
 
 
   
--- readme.txt
+++ readme.txt
@@ -1,13 +1,22 @@
 ===portfolio.csv===
-Date - Pos - Price
+Purchase Date - Pos (Amt of BTC) - Price (Cost Paid)
 
 
 !!!!! Good Data (numbers match), but cvs data type should be updated.
 
-===Functions.py===: (orphaned)
-
-UpdatePortfolio(csv) (not finished yet)
 
 ===DataPlay.R====:
-Basic code to update portfolio.csv
+Basic code utilizing portfolio.csv
 
+Begin():
+ portfolio = data frame of portfolio.cv (!!fix data types!!)
+ op = overall position
+ oi = overall investment
+ pb = price basis
+
+ pb_vec = vector of cumulative price basis by month (for use in graphs, not made yet)
+ #price, gotta do a bunch with price#
+ 
+Make better graph than https://docs.google.com/spreadsheets/d/1UCWkA3TnT3SGNOi8efVu7MfkNTx9ZzKQ01X3NIWeB2U/edit#gid=790779993
+
+===Functions.py===: (orphaned)
