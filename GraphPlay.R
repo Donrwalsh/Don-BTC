f1f2ee6ea237e9b8b81b7548ba3be3a050282a49
Donrwalsh <donrwalsh@gmail.com>
2015-05-12 15:28:07 -0500

    Fixed Graph() Function

--- GraphPlay.R
+++ GraphPlay.R
@@ -14,3 +14,25 @@ GraphYR <- function(){
 }
 
 #^Works. Data is proper (and rolling) and colors are good too. Axes are labeled, but the Time count is messed up.
+
+Graph <- function(months){
+  #verify input:
+  if (!is.numeric(months)){
+    stop("Invalid entry; Not a number")
+  }
+  if (months > length(pmo_high)){
+    stop("Invalid entry; Too many months")
+  }
+  #generate year & mon:
+  year <- as.numeric(format(Sys.Date(), "%Y")) - (floor(months/12))
+  mon <- as.numeric(format(Sys.Date(), "%m")) - (months %% 12)
+  high <- window(pmo_high, c(year, mon))
+  low <- window(pmo_low, c(year, mon))
+  if (months > length(mo_pricebasis)){
+    pbpb <- mo_pricebasis
+  } else {
+    pbpb <- window(mo_pricebasis, c(year, mon))
+  }
+  ts.plot(high, low, pbpb, gpars=list(xlab="Time", ylab="Price", col = c("red", "blue", "black")))
+}
+#^Works. Data is proper (and rolling) and colors are good too. Axes are labeled, but the Time count is messed up occasionally.
